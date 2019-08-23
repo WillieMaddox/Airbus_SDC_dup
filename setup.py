bb383e531893a4faade47721f435bcf46a60fa29
WillieMaddox <willie.maddox@gmail.com>
2019-08-23 08:00:19 -0500

    feat:  Add project scripts.
    Makefile, setup.py, tox.ini, and test_environment.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sdcdup',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Finding and tagging duplicate satellite images based on overlapping sub tiles.',
+    author='Willie Maddox',
+    license='MIT',
+)
