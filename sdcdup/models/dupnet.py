ae94a425721df68e97a7f7e24c0eac74d7191b71
WillieMaddox <willie.maddox@gmail.com>
2020-01-08 03:20:40 -0600

    refactor: clean up imports.

--- sdcdup/models/dupnet.py
+++ sdcdup/models/dupnet.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn
-import torch.nn.functional as F
 
 
 def save_checkpoint(filepath, model):
@@ -54,6 +53,7 @@ class DupCNN(nn.Module):
         self.pool = nn.MaxPool2d(kernel_size=2)
         # 16384 input features, 128 output features (see sizing flow below)
         self.fc1 = nn.Linear(256 * 8 * 8, 128)
+        self.relu = nn.ReLU()
         # 128 input features, 1 output feature
         self.fc2 = nn.Linear(128, output_size)
 
@@ -73,32 +73,32 @@ class DupCNN(nn.Module):
     def forward(self, x):
 
         # for each in self.conv_layers:
-        #     x = F.relu(each(x))
+        #     x = self.relu(each(x))
         #     x = self.pool(x)
         # x = x.view(-1, self.flat_size)
         # for each in self.fc_layers:
-        #     x = F.relu(each(x))
+        #     x = self.relu(each(x))
         # x = self.output(x)
         # return torch.sigmoid(x)
 
         x = self.conv1(x)  # Size changes from (6, 256, 256) to (16, 256, 256)
-        x = F.relu(x)  # Computes the activation of the first convolution
+        x = self.relu(x)  # Computes the activation of the first convolution
         x = self.pool(x)  # Size changes from (16, 256, 256) to (16, 128, 128)
 
         x = self.conv2(x)  # Size changes from (16, 128, 128) to (32, 128, 128)
-        x = F.relu(x)  # Computes the activation of the second convolution
+        x = self.relu(x)  # Computes the activation of the second convolution
         x = self.pool(x)  # Size changes from (32, 128, 128) to (32, 64, 64)
 
         x = self.conv3(x)  # Size changes from (32, 64, 64) to (64, 64, 64)
-        x = F.relu(x)  # Computes the activation of the third convolution
+        x = self.relu(x)  # Computes the activation of the third convolution
         x = self.pool(x)  # Size changes from (64, 64, 64) to (64, 32, 32)
 
         x = self.conv4(x)  # Size changes from (64, 32, 32) to (128, 32, 32)
-        x = F.relu(x)  # Computes the activation of the fourth convolution
+        x = self.relu(x)  # Computes the activation of the fourth convolution
         x = self.pool(x)  # Size changes from (128, 32, 32) to (128, 16, 16)
 
         x = self.conv5(x)  # Size changes from (128, 16, 16) to (256, 16, 16)
-        x = F.relu(x)  # Computes the activation of the fifth convolution
+        x = self.relu(x)  # Computes the activation of the fifth convolution
         x = self.pool(x)  # Size changes from (256, 16, 16) to (256, 8, 8)
 
         # Reshape data to input to the input layer of the neural net
@@ -106,8 +106,7 @@ class DupCNN(nn.Module):
         x = x.view(-1, 256 * 8 * 8)  # Size changes from (256, 8, 8) to (1, 16384)
 
         x = self.fc1(x)  # Size changes from (1, 16384) to (1, 128)
-        x = F.relu(x)  # Computes the activation of the first fully connected layer
-
+        x = self.relu(x)  # Computes the activation of the first fully connected layer
         x = self.fc2(x)  # Size changes from (1, 128) to (1, 1)
         x = torch.sigmoid(x)  # Computes the activation of the second fully connected layer
         return x
@@ -132,55 +131,34 @@ class DupCNN2(nn.Module):
         self.pool = nn.MaxPool2d(kernel_size=2)
         # 8192 input features, 256 output features (see sizing flow below)
         self.fc1 = nn.Linear(512 * 4 * 4, 256)
+        self.relu = nn.ReLU()
         # 128 input features, 1 output feature
         self.fc2 = nn.Linear(256, output_size)
 
-        # self.conv_layers = nn.ModuleList([nn.Conv2d(input_shape[0], conv_layers[0], kernel_size=3, stride=1, padding=1)])
-        # conv_layer_sizes = zip(conv_layers[:-1], conv_layers[1:])
-        # for h1, h2 in conv_layer_sizes:
-        #     self.conv_layers.extend([nn.Conv2d(h1, h2, kernel_size=3, stride=1, padding=1)])
-        # self.pool = nn.MaxPool2d(kernel_size=2)
-        # k_size = input_shape[1] >> len(conv_layers)
-        # self.flat_size = conv_layers[-1] * k_size * k_size
-        # self.fc_layers = nn.ModuleList([nn.Linear(self.flat_size, fc_layers[0])])
-        # if len(fc_layers) > 1:
-        #     fc_layer_sizes = zip(fc_layers[:-1], fc_layers[1:])
-        #     self.fc_layers.extend([nn.Linear(fc1, fc2) for fc1, fc2 in fc_layer_sizes])
-        # self.output = nn.Linear(fc_layers[-1], output_size)
-
     def forward(self, x):
 
-        # for each in self.conv_layers:
-        #     x = F.relu(each(x))
-        #     x = self.pool(x)
-        # x = x.view(-1, self.flat_size)
-        # for each in self.fc_layers:
-        #     x = F.relu(each(x))
-        # x = self.output(x)
-        # return torch.sigmoid(x)
-
         x = self.conv1(x)  # Size changes from (6, 256, 256) to (16, 256, 256)
-        x = F.relu(x)  # Computes the activation of the first convolution
+        x = self.relu(x)  # Computes the activation of the first convolution
         x = self.pool(x)  # Size changes from (16, 256, 256) to (16, 128, 128)
 
         x = self.conv2(x)  # Size changes from (16, 128, 128) to (32, 128, 128)
-        x = F.relu(x)  # Computes the activation of the second convolution
+        x = self.relu(x)  # Computes the activation of the second convolution
         x = self.pool(x)  # Size changes from (32, 128, 128) to (32, 64, 64)
 
         x = self.conv3(x)  # Size changes from (32, 64, 64) to (64, 64, 64)
-        x = F.relu(x)  # Computes the activation of the third convolution
+        x = self.relu(x)  # Computes the activation of the third convolution
         x = self.pool(x)  # Size changes from (64, 64, 64) to (64, 32, 32)
 
         x = self.conv4(x)  # Size changes from (64, 32, 32) to (128, 32, 32)
-        x = F.relu(x)  # Computes the activation of the fourth convolution
+        x = self.relu(x)  # Computes the activation of the fourth convolution
         x = self.pool(x)  # Size changes from (128, 32, 32) to (128, 16, 16)
 
         x = self.conv5(x)  # Size changes from (128, 16, 16) to (256, 16, 16)
-        x = F.relu(x)  # Computes the activation of the fifth convolution
+        x = self.relu(x)  # Computes the activation of the fifth convolution
         x = self.pool(x)  # Size changes from (256, 16, 16) to (256, 8, 8)
 
         x = self.conv6(x)  # Size changes from (256, 8, 8) to (512, 8, 8)
-        x = F.relu(x)  # Computes the activation of the fifth convolution
+        x = self.relu(x)  # Computes the activation of the fifth convolution
         x = self.pool(x)  # Size changes from (512, 8, 8) to (512, 4, 4)
 
         # Reshape data to input to the input layer of the neural net
@@ -188,8 +166,7 @@ class DupCNN2(nn.Module):
         x = x.view(-1, 512 * 4 * 4)  # Size changes from (512, 4, 4) to (1, 8192)
 
         x = self.fc1(x)  # Size changes from (1, 16384) to (1, 128)
-        x = F.relu(x)  # Computes the activation of the first fully connected layer
-
+        x = self.relu(x)  # Computes the activation of the first fully connected layer
         x = self.fc2(x)  # Size changes from (1, 128) to (1, 1)
         x = torch.sigmoid(x)  # Computes the activation of the second fully connected layer
         return x
